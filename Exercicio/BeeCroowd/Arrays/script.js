d67a0f34ea64c1dbf25bcf735de60ea952c89c49
Nahuel Omar Gauna <nahuelgau@gmail.com>
2024-10-03 22:05:16 -0300

    Add files via upload

---
+++ Exercicio/BeeCroowd/Arrays/script.js
@@ -0,0 +1,11 @@
+//let Melancia = "Bom"
+let idades = [45, 22, 34, 0]
+
+function testar(){
+
+    //console.log(idades);
+    //let nome = prompt("Digite seu nome: ")
+    // console.log(nome);
+    
+}
+//alert ("Melancia")
